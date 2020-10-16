70c75b9a050272e2e811739cec562de725c73d13
NikRuchkin1 <prolive419000@gmail.com>
2020-10-16 23:22:44 +0300

    -

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 
 function Header() {
-  return <div></div>;
+  return (
+    <div className="container">
+      <div className="user_header">
+        <NotificationsNoneIcon />
+      </div>
+    </div>
+  );
 }
 
 export default Header;
