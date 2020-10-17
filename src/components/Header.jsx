f84d12c7bf372e524171ced31826e8d77e893e6b
NikRuchkin1 <prolive419000@gmail.com>
2020-10-17 20:51:03 +0300

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles((theme) => ({
+  icon: {
+    color: '#fff',
+    width: 24,
+    height: 29.25,
+  },
+}));
 
 function Header() {
+  const style = useStyles()
   return (
     <div className="container">
       <div className="user_header">
-        <NotificationsNoneIcon />
+        <NotificationsNoneIcon fontSize='medium' className={style.icon}/>
+        <span className='line_dividing'/>
       </div>
     </div>
   );
