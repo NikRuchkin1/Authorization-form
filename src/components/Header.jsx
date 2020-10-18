976510c2f5d0b01f564dc43e5fdf8ef20de6f740
NikRuchkin1 <prolive419000@gmail.com>
2020-10-18 23:22:26 +0300

    added new components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,24 +1,35 @@
 import React from 'react';
+import '../scss/Header.scss'
 import NotificationsNoneIcon from '@material-ui/icons/NotificationsNone';
 import { makeStyles } from '@material-ui/core';
+import maskGroup from '../components/icons/MaskGroup.png'
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
   icon: {
     color: '#fff',
-    width: 24,
-    height: 29.25,
+    marginRight: 26,
   },
 }));
 
 function Header() {
   const style = useStyles()
   return (
-    <div className="container">
-      <div className="user_header">
-        <NotificationsNoneIcon fontSize='medium' className={style.icon}/>
-        <span className='line_dividing'/>
+      <div className='user_header'>
+        <div className='userHeaderBox'>
+          <div className='svg'>
+            <NotificationsNoneIcon fontSize='large' className={style.icon}/>
+          </div>
+          <div className='line'>
+            <span className='line_dividing'/>
+          </div>
+          <div className='userImage'>
+            <img src={maskGroup} alt='#'/>
+          </div>
+          <div className='userName'>
+            Иванова А.
+          </div>
+        </div>
       </div>
-    </div>
   );
 }
 
