dde3bafc1bc3c803a8d1045b68278999f4bcacc0
NikRuchkin1 <prolive419000@gmail.com>
2020-10-26 23:24:50 +0300

    -

---
+++ src/redux/reducer/formReducer.js
@@ -0,0 +1,22 @@
+const initialState = {
+    name: "Иванова Анна Михайловна",
+    nameHead: "Иванова А.",
+    id: 0,
+    email: "Укажите e-mail",
+    phone: "Укажите номер телефона"
+  };
+
+  const user = (state = initialState, action) => {
+    switch (action.type) {
+        case 'SET_USER_INFO':
+          return {
+            ...state,
+            name: action.name,
+            email: action.email,
+            phone: action.phone
+          };
+          default:
+    return state}
+  };
+
+  export default user;
